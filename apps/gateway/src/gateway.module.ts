4a292aed5219bd83807cccb109b654f80792d32c
Deon Sanchez <deon.sanchez@datastax.com>
2024-02-08 18:25:19 -0700

    Add new files and update existing files

---
+++ apps/gateway/src/gateway.module.ts
@@ -0,0 +1,40 @@
+import { IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { authContext } from './auth.context';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      server: {
+        context: authContext,
+      },
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            {
+              name: 'users',
+              url: 'http://localhost:3001/graphql',
+            },
+          ],
+        }),
+        buildService({ url }) {
+          return new RemoteGraphQLDataSource({
+            url,
+            willSendRequest({ request, context }) {
+              request.http.headers.set(
+                'user',
+                context.user ? JSON.stringify(context.user) : null,
+              );
+            },
+          });
+        },
+      },
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class GatewayModule {}
---
+++ apps/users/src/users.resolver.ts
@@ -0,0 +1,47 @@
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  Int,
+  ResolveReference,
+} from '@nestjs/graphql';
+import { UsersService } from './users.service';
+import { User } from './entities/user.entity';
+import { CreateUserInput } from './dto/create-user.input';
+import { UpdateUserInput } from './dto/update-user.input';
+
+@Resolver(() => User)
+export class UsersResolver {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Mutation(() => User)
+  createUser(@Args('createUserInput') createUserInput: CreateUserInput) {
+    return this.usersService.create(createUserInput);
+  }
+
+  @Query(() => [User], { name: 'users' })
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Query(() => User, { name: 'user' })
+  findOne(@Args('email', { type: () => String }) email: string) {
+    return this.usersService.findOne(email);
+  }
+
+  @Mutation(() => User)
+  updateUser(@Args('updateUserInput') updateUserInput: UpdateUserInput) {
+    return this.usersService.update(updateUserInput.id, updateUserInput);
+  }
+
+  @Mutation(() => User)
+  removeUser(@Args('id', { type: () => Int }) id: number) {
+    return this.usersService.remove(id);
+  }
+
+  @ResolveReference()
+  resolveReference(reference: { __typename: string; email: string }) {
+    return this.usersService.findOne(reference.email);
+  }
+}
